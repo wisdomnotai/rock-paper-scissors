9f071826ad232db432f0a27a73407afbecb34c70
Wisdom Alawode <alawodewisdom02@gmail.com>
2026-09-08 22:23:33 +0100

    stage 1: creating the getComputerChoice function

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
-console.log("Hello, World!");
+//rock paper scissors game
+//creating a get computer choice function
+
+const getComputerChoice = () => {
+    const choiceNumber = Math.random();
+    if (choiceNumber >= 0 && choiceNumber <= 0.333){
+        return "rock";
+    }
+    else if (choiceNumber > 0.333 && choiceNumber <= 0.666){
+        return "paper";
+    }
+    else{
+        return "scissors";
+    }
+}
